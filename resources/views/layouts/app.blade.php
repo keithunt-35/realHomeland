4c77b5de9c77d81e8dacdacca605c6f4ebb58e65
keithunt-35 <keithunt35@duck.com>
2025-03-22 15:59:21 +0300

    ui in app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <!-- imported css files -->
     
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Roboto+Mono:300,400,500"> 
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="{{asfonts/icomoon/style.css">
 
     <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
@@ -97,5 +97,21 @@
             @yield('content')
         </main>
     </div>
+
+    
+  <script src="{{asset('assets/js/jquery-3.3.1.min.js')}}"></script>
+  <script src="{{asset('assets/js/jquery-migrate-3.0.1.min.js')}}"></script>
+  <script src="{{asset('assets/js/jquery-ui.js')}}"></script>
+  <script src="{{asset('assets/js/popper.min.js')}}"></script> 
+  <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+  <script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+  <script src="{{asset('assets/js/mediaelement-and-player.min.js')}}"></script>
+  <script src="{{asset('assets/js/jquery.stellar.min.js')}}"></script>
+  <script src="{{asset('assets/js/jquery.countdown.min.js')}}"></script>
+  <script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+  <script src="{{asset('assets/js/bootstrap-datepicker.min.js')}}"></script>
+  <script src="{{asset('assets/js/aos.js')}}"></script>
+
+  <script src="{{asset('assets/js/main.js')}}"></script>
 </body>
 </html>
